b76f923a75d6e631c54e593cef953e23b37e1c58
franklingu <franklingujunchao@gmail.com>
2015-09-22 22:04:01 +0800

    setup script to take 2 arguments; deal with null value
    null value is handled with wtih command. however, for
    d40, most likely cqlsh may encounter some problem.
    let us wait and see
    Close #12
    Close #13

--- README.md
+++ README.md
@@ -6,11 +6,10 @@ This is a benchmarking of different modeling of the same data set and same query
 
 ### Setup
 1. Get Cassandra installed first by following the instructions [on datastax](http://docs.datastax.com/en/cassandra/2.1/cassandra/install/install_cassandraTOC.html)
-2. Run ```./setup.sh``` for database creation and data importing.
+2. Run ```./setup.sh``` for database creation and data importing. For more info on setup.sh, you can take a look at different arguments it takes and what those arguments mean.
 
 ### Benchmarking
 To be continued
 
 ### Analysis
 To be continued
-
--- setup.sh
+++ setup.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
+
+# This script takes into 2 arguments: 1st for performing download or not;
+#   2nd tries to setup database based on the selected datasource
+
 if [ ! -d "data" ]
 then
   mkdir data
 else
-  echo "data folder exists already"
+  echo "Data folder exists already"
+fi
+data_op=$2
+if [ $data_op == "D40" ]
+then
+  echo "Selected datasource ${data_op}"
+else
+  data_op="D8"
+  echo "Selected datasource ${data_op}"
 fi
 cd data
-wget http://www.comp.nus.edu.sg/~cs4224/D8.zip -O D8.zip
-if [ -d "D8" ]
+if [ $1 == "true" ]
 then
-  rm -rf D8
+  wget http://www.comp.nus.edu.sg/~cs4224/D8.zip -O D8.zip
+  if [ -d $data_op ]
+  then
+    rm -rf $data_op
+  fi
+  unzip ${data_op}.zip
 fi
-unzip D8.zip
-cd D8
+cd $data_op
 cqlsh -f ../../schema.cql
