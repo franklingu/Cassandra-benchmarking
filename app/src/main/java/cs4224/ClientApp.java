35ba3fa2ba16b6443766f074081ed882d9c3b7a3
Jiang Sheng <gisonrg@gmail.com>
2015-10-20 00:59:17 +0800

    Try and catch errors.

--- app/src/main/java/cs4224/ClientApp.java
+++ app/src/main/java/cs4224/ClientApp.java
@@ -69,51 +69,81 @@ public class ClientApp {
                         supplierWarehouse[j] = Integer.parseInt(newParams[1]);
                         quantity[j] = Integer.parseInt(newParams[2]);
                     }
-                    startTime = System.nanoTime();
-                    n.createOrder(wId, dId, cId, numItems, itemNumbers, supplierWarehouse, quantity);
-                    timings[0] = timings[0] + (System.nanoTime() - startTime);
-                    transactionCounts[0] = transactionCounts[0] + 1;
+
+                    try {
+                        startTime = System.nanoTime();
+                        n.createOrder(wId, dId, cId, numItems, itemNumbers, supplierWarehouse, quantity);
+                        timings[0] = timings[0] + (System.nanoTime() - startTime);
+                        transactionCounts[0] = transactionCounts[0] + 1;
+                    } catch (Exception e) {
+                        System.err.println(e.getStackTrace());
+                    }
                 } else if (inputLine.charAt(0) == 'P') {
                     int wId = Integer.parseInt(params[1]);
                     int dId = Integer.parseInt(params[2]);
                     int cId = Integer.parseInt(params[3]);
                     float payment = Float.parseFloat(params[4]);
-                    startTime = System.nanoTime();
-                    p.processPayment(wId, dId, cId, payment);
-                    timings[1] = timings[1] + (System.nanoTime() - startTime);
-                    transactionCounts[1] = transactionCounts[1] + 1;
+
+                    try {
+                        startTime = System.nanoTime();
+                        p.processPayment(wId, dId, cId, payment);
+                        timings[1] = timings[1] + (System.nanoTime() - startTime);
+                        transactionCounts[1] = transactionCounts[1] + 1;
+                    } catch (Exception e) {
+                        System.err.println(e.getStackTrace());
+                    }
                 } else if (inputLine.charAt(0) == 'D') {
                     int wId = Integer.parseInt(params[1]);
                     int carrierId = Integer.parseInt(params[2]);
-                    startTime = System.nanoTime();
-                    d.executeQuery(wId, carrierId);
-                    timings[2] = timings[2] + (System.nanoTime() - startTime);
-                    transactionCounts[2] = transactionCounts[2] + 1;
+
+                    try {
+                        startTime = System.nanoTime();
+                        d.executeQuery(wId, carrierId);
+                        timings[2] = timings[2] + (System.nanoTime() - startTime);
+                        transactionCounts[2] = transactionCounts[2] + 1;
+                    } catch (Exception e) {
+                        System.err.println(e.getStackTrace());
+                    }
                 } else if (inputLine.charAt(0) == 'O') { // Order Status
                     int wId = Integer.parseInt(params[1]);
                     int dId = Integer.parseInt(params[2]);
                     int cId = Integer.parseInt(params[3]);
-                    startTime = System.nanoTime();
-                    o.getOrderStatus(wId, dId, cId);
-                    timings[3] = timings[3] + (System.nanoTime() - startTime);
-                    transactionCounts[3] = transactionCounts[3] + 1;
+
+                    try {
+                        startTime = System.nanoTime();
+                        o.getOrderStatus(wId, dId, cId);
+                        timings[3] = timings[3] + (System.nanoTime() - startTime);
+                        transactionCounts[3] = transactionCounts[3] + 1;
+                    } catch (Exception e) {
+                        System.err.println(e.getStackTrace());
+                    }
                 } else if (inputLine.charAt(0) == 'S') {
                     int wId = Integer.parseInt(params[1]);
                     int dId = Integer.parseInt(params[2]);
                     int T = Integer.parseInt(params[3]);
                     int L = Integer.parseInt(params[4]);
-                    startTime = System.nanoTime();
-                    s.executeQuery(wId, dId, T, L);
-                    timings[4] = timings[4] + (System.nanoTime() - startTime);
-                    transactionCounts[4] = transactionCounts[4] + 1;
+
+                    try {
+                        startTime = System.nanoTime();
+                        s.executeQuery(wId, dId, T, L);
+                        timings[4] = timings[4] + (System.nanoTime() - startTime);
+                        transactionCounts[4] = transactionCounts[4] + 1;
+                    } catch (Exception e) {
+                        System.err.println(e.getStackTrace());
+                    }
                 } else if (inputLine.charAt(0) == 'I') {
                     int wId = Integer.parseInt(params[1]);
                     int dId = Integer.parseInt(params[2]);
                     int L = Integer.parseInt(params[3]);
-                    startTime = System.nanoTime();
-                    popular.findItem(wId, dId, L);
-                    timings[5] = timings[5] + (System.nanoTime() - startTime);
-                    transactionCounts[5] = transactionCounts[5] + 1;
+
+                    try {
+                        startTime = System.nanoTime();
+                        popular.findItem(wId, dId, L);
+                        timings[5] = timings[5] + (System.nanoTime() - startTime);
+                        transactionCounts[5] = transactionCounts[5] + 1;
+                    } catch (Exception e) {
+                        System.err.println(e.getStackTrace());
+                    }
                 } else {
                     System.err.println("\n\nSeems the way of reading of file is wrong\n\n");
                 }
