756b0b50b0e082591fa388dc8ed4c83b1bb75c06
franklingu <franklingujunchao@gmail.com>
2015-10-21 18:47:07 +0800

    modified launcher script for new working

--- app/launcher.sh
+++ app/launcher.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+mvn install
+mvn build
+rm -rf log
+mkdir log
+echo "Starting simulating clients..."
 for i in `seq 1 3`; do
     mvn exec:java -Dexec.args="D8 $i" 1> log/out_$i.log 2> log/err_$i.log &
 done
+wait
+./compile_results.sh
+echo "Done with simulation"
