17e1b676ac9dfe4412e97e808f7be990c65e449b
Jason Jiang <gisonrg@gmail.com>
2015-10-23 16:03:15 +0800

    Update ClientApp's trace errors.

--- app/src/main/java/cs4224/ClientApp.java
+++ app/src/main/java/cs4224/ClientApp.java
@@ -76,7 +76,7 @@ public class ClientApp {
                         timings[0] = timings[0] + (System.nanoTime() - startTime);
                         transactionCounts[0] = transactionCounts[0] + 1;
                     } catch (Exception e) {
-                        System.err.println(e.getStackTrace());
+                        System.err.println(e.getMessage());
                     }
                 } else if (inputLine.charAt(0) == 'P') {
                     int wId = Integer.parseInt(params[1]);
@@ -90,7 +90,7 @@ public class ClientApp {
                         timings[1] = timings[1] + (System.nanoTime() - startTime);
                         transactionCounts[1] = transactionCounts[1] + 1;
                     } catch (Exception e) {
-                        System.err.println(e.getStackTrace());
+                        System.err.println(e.getMessage());
                     }
                 } else if (inputLine.charAt(0) == 'D') {
                     int wId = Integer.parseInt(params[1]);
@@ -102,7 +102,7 @@ public class ClientApp {
                         timings[2] = timings[2] + (System.nanoTime() - startTime);
                         transactionCounts[2] = transactionCounts[2] + 1;
                     } catch (Exception e) {
-                        System.err.println(e.getStackTrace());
+                        System.err.println(e.getMessage());
                     }
                 } else if (inputLine.charAt(0) == 'O') { // Order Status
                     int wId = Integer.parseInt(params[1]);
@@ -115,7 +115,7 @@ public class ClientApp {
                         timings[3] = timings[3] + (System.nanoTime() - startTime);
                         transactionCounts[3] = transactionCounts[3] + 1;
                     } catch (Exception e) {
-                        System.err.println(e.getStackTrace());
+                        System.err.println(e.getMessage());
                     }
                 } else if (inputLine.charAt(0) == 'S') {
                     int wId = Integer.parseInt(params[1]);
@@ -129,7 +129,7 @@ public class ClientApp {
                         timings[4] = timings[4] + (System.nanoTime() - startTime);
                         transactionCounts[4] = transactionCounts[4] + 1;
                     } catch (Exception e) {
-                        System.err.println(e.getStackTrace());
+                        System.err.println(e.getMessage());
                     }
                 } else if (inputLine.charAt(0) == 'I') {
                     int wId = Integer.parseInt(params[1]);
@@ -142,7 +142,7 @@ public class ClientApp {
                         timings[5] = timings[5] + (System.nanoTime() - startTime);
                         transactionCounts[5] = transactionCounts[5] + 1;
                     } catch (Exception e) {
-                        System.err.println(e.getStackTrace());
+                        System.err.println(e.getMessage());
                     }
                 } else {
                     System.err.println("\n\nSeems the way of reading of file is wrong\n\n");
