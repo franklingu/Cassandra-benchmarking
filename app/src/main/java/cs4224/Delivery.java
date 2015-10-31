3f1ffd0ca2d41519e153f94f72eb85d042a40471
franklingu <franklingujunchao@gmail.com>
2015-10-31 22:58:18 +0800

    added implementation to delivery
    finished both of my transactions

--- app/src/main/java/cs4224/Delivery.java
+++ app/src/main/java/cs4224/Delivery.java
@@ -8,18 +8,17 @@ public class Delivery {
     private Session session;
     private PreparedStatement selectMinOIdQuery;
     private PreparedStatement updateCarrierIdQuery;
-    private PreparedStatement selectOlNumberQuery;
     private PreparedStatement updateDeliveryDateQuery;
     private PreparedStatement selectBalanceCntQuery;
     private PreparedStatement updateBalanceCntQuery;
 
 
     public static void main(String[] args) {
-        int inputinputWId = 1, inputCarrierId = 7;
+        int inputWId = 1, inputCarrierId = 7;
         SimpleClient client = new SimpleClient();
         client.connect("127.0.0.1", "cs4224");
         Delivery d = new Delivery(client);
-        d.executeQuery(inputinputWId, inputCarrierId);
+        d.executeQuery(inputWId, inputCarrierId);
         client.close();
     }
 
@@ -30,10 +29,9 @@ public class Delivery {
                 + "FROM Orders where o_w_id = ? AND o_d_id = ? AND o_carrier_id = 0;");
         this.updateCarrierIdQuery = session.prepare("UPDATE Orders SET o_carrier_id = ? WHERE o_w_id = ? AND o_d_id = ?"
                 + " AND o_id = ?;");
-        this.selectOlNumberQuery = session.prepare("SELECT ol_number, ol_amount FROM OrderLines WHERE ol_w_id = ? AND ol_d_id = ?"
-                + " AND ol_o_id = ?;");
-        this.updateDeliveryDateQuery = session.prepare("UPDATE OrderLines SET ol_delivery_d = ? WHERE ol_w_id = ? AND ol_d_id = ?"
-                + " AND ol_o_id = ? AND ol_number = ?;");
+        this.updateDeliveryDateQuery = session.prepare("UPDATE orders set o_ols[?] = {OL_I_ID: ?," +
+                " OL_DELIVERY_D: ?, OL_AMOUNT: ?, OL_SUPPLY_W_ID: ?, OL_QUANTITY: ?, OL_DIST_INFO: ?} where o_w_id = ?" +
+                " and o_d_id = ? and o_id = ?;");
         this.selectBalanceCntQuery = session.prepare("SELECT c_balance, c_delivery_cnt FROM Customers WHERE c_w_id = ? AND c_d_id = ?"
                 + " AND c_id = ?;");
         this.updateBalanceCntQuery = session.prepare("UPDATE Customers SET c_balance = ?, c_delivery_cnt = ? WHERE c_w_id = ? AND c_d_id = ?"
@@ -41,37 +39,34 @@ public class Delivery {
     }
 
     public void executeQuery(int inputWId, int inputCarrierId) {
-        ResultSet results = session.execute(selectMinOIdQuery.bind(inputWId, 1));
-        for (Row row: results) {
-            Map<Integer, UDTValue> ols = row.getMap("o_ols", Integer.class, UDTValue.class);
-            for (Integer i: ols.keySet()) {
-                UDTValue is = ols.get(i);
-                System.out.println(is.toString());
-            }
-        }
         for (int i = 1; i <= 10; i++) {
-            int dId = i, cId = 0;
-            results = session.execute(selectMinOIdQuery.bind(inputWId, dId));
-            int minOId = 0;
-            for (Row row : results) {
-                minOId = row.getInt("min_o_id");
-                cId = row.getInt("o_c_id");
-                break;
-            }
-            // System.out.format("MinOId: %d, CId: %d\n", minOId, cId);
-
-            session.execute(updateCarrierIdQuery.bind(inputCarrierId, inputWId, dId, minOId));
-
-            Date now = new Date();
-
-            results = session.execute(selectOlNumberQuery.bind(inputWId, dId, minOId));
+            int dId = i;
+            int minOId = 0, cId = 0;
             float olSum = 0;
-            int olNumber = 0;
-            for (Row row : results) {
-                olSum += row.getFloat("ol_amount");
-                olNumber = row.getInt("ol_number");
-                // System.out.format("OLNumber: %d\n", olNumber);
-                session.execute(updateDeliveryDateQuery.bind(new Timestamp(now.getTime()), inputWId, dId, minOId, olNumber));
+            int olNumber, olQuantity, olSupplyWId, olIId;
+            float olAmount;
+            String olDistInfo;
+            ResultSet results = session.execute(selectMinOIdQuery.bind(inputWId, dId));
+            Date now = new Date();
+            for (Row row: results) {
+                minOId = row.getInt("min_o_id");
+                System.out.format("%d\n", minOId);
+                cId = row.getInt("o_c_id");
+                session.execute(updateCarrierIdQuery.bind(inputCarrierId, inputWId, dId, minOId));
+                Map<Integer, UDTValue> ols = row.getMap("o_ols", Integer.class, UDTValue.class);
+                for (Integer key: ols.keySet()) {
+                    UDTValue ol = ols.get(key);
+                    olNumber = key.intValue();
+                    olIId = ol.getInt("ol_i_id");
+                    olQuantity = ol.getInt("ol_quantity");
+                    olDistInfo = ol.getString("ol_dist_info");
+                    olSupplyWId = ol.getInt("ol_supply_w_id");
+                    olAmount = ol.getFloat("ol_amount");
+                    olSum += olAmount;
+                    session.execute(updateDeliveryDateQuery.bind(olNumber, olIId, new Timestamp(now.getTime()),
+                            olAmount, olSupplyWId, olQuantity, olDistInfo, inputWId, dId, minOId));
+                }
+                break;
             }
 
             float cBalance = 0;
@@ -81,7 +76,6 @@ public class Delivery {
                 cBalance = row.getFloat("c_balance");
                 cCnt = row.getInt("c_delivery_cnt");
             }
-            // System.out.format("CBalance: %f, cCnt: %d\n", cBalance, cCnt);
             cBalance += olSum;
             cCnt++;
             session.execute(updateBalanceCntQuery.bind(cBalance, cCnt, inputWId, dId, cId));
